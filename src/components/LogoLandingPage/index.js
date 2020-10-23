f12d6c32aaa795bbd77b73c65988328b5907b13d
Alex Shortt <alexander.shortt@gmail.com>
2020-10-22 22:06:36 -0700

    switch shadow to not shadow

--- src/components/LogoLandingPage/index.js
+++ src/components/LogoLandingPage/index.js
@@ -1,14 +1,16 @@
 /* eslint-disable jsx-a11y/no-noninteractive-tabindex */
 import React from "react"
 
-import PorcelainLogo from "../Images/Porcelain_Title.png"
+import PorcelainLogo from "../Images/porcelain.png"
+import PorcelainShadow from "../Images/porcelain_shadow.png"
 
 import "./styles.scss"
 
-export default function Logo() {
+export default function Logo(props) {
   return (
     <div className="logo-container">
-      <img className="logo" src={PorcelainLogo} alt="Porcelain" />
+      <img className="logo" src={PorcelainShadow} alt="Porcelain" />
+      <img className="logo no-shadow" src={PorcelainLogo} alt="Porcelain" />
     </div>
   )
 }
